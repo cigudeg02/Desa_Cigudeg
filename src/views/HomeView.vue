37ea8f33ebdd52ce460a11a1d4f3d71aca7b54f5
cigudeg02 <149566953+cigudeg02@users.noreply.github.com>
2023-11-01 14:50:50 +0700

    Update HomeView.vue
    update desa

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,9 +15,7 @@ import Sidebar from "../components/Sidebar.vue";
       <p class="text-3xl font-semibold">Visi & Misi Desa Cigudeg</p>
       <p class="text-lg font-semibold">Visi</p>
       <p>
-        “Terwujudnya Desa Setu Yang Sejahtera dan Mandiri, ditopang Kinerja
-        Aparatur yang Profesional dalam Kerangka Budaya Bangsa didasari Keimanan
-        dan Ketaqwaan”
+        “Terwujudnya masyarakat Desa Cigudeg yang beriman dan bertaqwa serta mempunyai kepribadian sosial yang tinggi”
       </p>
       <p class="text-lg font-semibold">Misi</p>
       <ul>
